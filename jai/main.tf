23b40b7794b870987b5b2971498971669c2cd406
Jayachandran <159675826+jai3747@users.noreply.github.com>
2024-07-22 15:56:53 +0530

    Update main.tf

--- jai/main.tf
+++ jai/main.tf
@@ -41,8 +41,10 @@ resource "aws_route_table" "rt" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_internet_gateway.gw.id
   }
+}
+
 
   tags = {
     Name = "main-rt"
