55fb43c7b1037ae38614c031c03d9a1588f16db3
Jayachandran <159675826+jai3747@users.noreply.github.com>
2024-07-22 14:55:23 +0530

    Update backend.tf

--- jai/backend.tf
+++ jai/backend.tf
@@ -2,5 +2,5 @@ terraform {
   backend "s3" {
     bucket         = "terraform.test.jai1"
     key            = "terraform/state"
-    region         = "eu-north-1"
+    region         = "ap-south-1"
   }
