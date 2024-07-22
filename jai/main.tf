27726814100afd41f37855d9c2677a2b873344ff
Jayachandran <159675826+jai3747@users.noreply.github.com>
2024-07-22 17:36:21 +0530

    Update main.tf

--- jai/main.tf
+++ jai/main.tf
@@ -1,7 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.59.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
-
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
 
