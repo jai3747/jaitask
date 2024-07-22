df6b6034487476f4ebd30011c61e48ca4681d100
Jayachandran <159675826+jai3747@users.noreply.github.com>
2024-07-22 15:48:06 +0530

    Update backend.tf

--- jai/backend.tf
+++ jai/backend.tf
@@ -3,4 +3,8 @@ terraform {
     bucket         = "terraform.test.jai1"
     key            = "terraform/state"
     region         = "ap-south-1"
+    access_key     = "AKIATCKAPVUR3KGLFKWT"
+    secret_key     = "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"
   }
+}
+
